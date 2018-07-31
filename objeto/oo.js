c369cdc10cf76e6871bd6a77f3a6fcfd79e94950
António Pinho <antonio.henrique@ua.pt>
2018-07-31 23:40:35 +0100

    objeto_exerc_86-87

---
+++ objeto/oo.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 6 */
+
+// CÓDIGO NÃO EXECUTÁVEL
+
+// procedural
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+};
+
+object.processamento(); // foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo

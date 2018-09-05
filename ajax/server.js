fdb2fe8df30b1e68f85b70f05b294ba407779675
António Pinho <antonio.henrique@ua.pt>
2018-09-06 00:22:18 +0100

    Ajax_Exerc_254-257

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,35 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({storage}).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro.');
+        }
+
+        res.end('Concluido com sucesso.');
+    })
+});
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
 app.get('/teste', (req, res) => res.send('Ok ==> ' + new Date));
 app.listen(8080, () => console.log('Executando...'));
